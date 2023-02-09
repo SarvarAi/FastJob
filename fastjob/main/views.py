59b53c33024945db98959e047a785e45aa7e6ca3
SarvarAi <sarvarislamov001@gmail.com>
2023-02-09 12:33:16 +0500

    Rewriting function to the Class-based and new design

--- fastjob/main/views.py
+++ fastjob/main/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
 # Create your views here.
-def index(request):
-    context = {
+# def index(request):
+#     context = {
+#         'title': 'FastJob'
+#     }
+#     return render(request, 'main/index.html', context=context)
+
+
+class Home(TemplateView):
+    extra_context = {
         'title': 'FastJob'
     }
-    return render(request, 'main/index.html', context=context)
+    template_name = 'main/index.html'
+
